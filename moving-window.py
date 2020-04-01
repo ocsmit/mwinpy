f36169b0d03d50a61c1a4b5f35c3268630cf3fb2
ocsmit <owen8461@protonmail.com>
2020-04-01 10:48:39 -0400

    Change window
    Window size now determined by one number

--- moving-window.py
+++ moving-window.py
@@ -14,7 +14,7 @@ def moving_window(array_1, array_2, window):
     for i in range(array_1.shape[0]):
         for j in range(array_1.shape[1]):
             height, width = array_1.shape
-            tw = (height * width) / (window[0] * window[1])
+            tw = (height * width) / (window * window)
             s = sum([q for q in range(math.ceil(tw))])
             p = sum([array_1[i, j]-array_2[i, j] for q in range(
                      height * width)])
