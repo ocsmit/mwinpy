f37507f3a25405f02d2f9e73d2f85cc886d3065b
ocsmit <owen8461@protonmail.com>
2020-10-27 23:17:20 -0400

    Method to account for nodata cells in raster, set by user

--- mwinpy/multi_mw.py
+++ mwinpy/multi_mw.py
@@ -21,10 +21,10 @@ class load_data:
 
 class MWin:
 
-    def __init__(self, threads, shape, window):
+    def __init__(self, threads, shape, window, nodata=128):
 
         self.i, self.j = shape
-
+        self.nodata = nodata
         self.w = window
         self.threads = threads
         self.cluster = self.i // threads
@@ -62,17 +62,23 @@ class MWin:
         n = im[i - d:i + d + 1, j - d:j + d + 1].flatten()
         return n
 
+    def __mw(self, arr1, arr2, ii, j, w):
+        if arr1[ii][j] and arr2[ii][j] == self.nodata:
+            return -1
+        else:
+            a = self.__neighbors(arr1, ii, j, self.w)
+            b = self.__neighbors(arr2, ii, j, self.w)
+            c = abs(a - b)
+            d = len(np.nonzero(c)[0])
+            e = abs((1 - d / w)) if d != 0 else w / w
+            return e
+
     def moving_window(self, arr1, arr2):
         w = (((self.w * 2) + 1) ** 2)
         vector = []
         for ii in range(arr1.shape[0]):
             for j in range(arr1.shape[1]):
-                a = self.__neighbors(arr1, ii, j, self.w)
-                b = self.__neighbors(arr2, ii, j, self.w)
-                c = abs(a - b)
-                d = len(np.nonzero(c)[0])
-                e = abs((1 - d / w)) if d != 0 else w / w
-                vector.append(e)
+                vector.append(self.__mw(arr1, arr2, ii, j, w))
 
         return vector
 
@@ -84,31 +90,16 @@ class MWin:
         if sl[0] == 0:
             for ii in range(arr1_sl.shape[0] - self.__d):
                 for j in range(arr1_sl.shape[1]):
-                    a = self.__neighbors(arr1_sl, ii, j, self.w)
-                    b = self.__neighbors(arr2_sl, ii, j, self.w)
-                    c = abs(a - b)
-                    d = len(np.nonzero(c)[0])
-                    e = abs((1 - d / w)) if d != 0 else w / w
-                    vector.append(e)
+                    vector.append(self.__mw(arr1_sl, arr2_sl, ii, j, w))
 
         elif sl[0] == self.threads - 1:
             for ii in range(self.__d, arr1_sl.shape[0]):
                 for j in range(arr1_sl.shape[1]):
-                    a = self.__neighbors(arr1_sl, ii, j, self.w)
-                    b = self.__neighbors(arr2_sl, ii, j, self.w)
-                    c = abs(a - b)
-                    d = len(np.nonzero(c)[0])
-                    e = abs((1 - d / w)) if d != 0 else w / w
-                    vector.append(e)
+                    vector.append(self.__mw(arr1_sl, arr2_sl, ii, j, w))
         else:
             for ii in range(self.__d, arr1_sl.shape[0] - (self.__d)):
                 for j in range(arr1_sl.shape[1]):
-                    a = self.__neighbors(arr1_sl, ii, j, self.w)
-                    b = self.__neighbors(arr2_sl, ii, j, self.w)
-                    c = abs(a - b)
-                    d = len(np.nonzero(c)[0])
-                    e = abs((1 - d / w)) if d != 0 else w / w
-                    vector.append(e)
+                    vector.append(self.__mw(arr1_sl, arr2_sl, ii, j, w))
         return vector
 
     def fit(self, arr, arr2):
@@ -122,20 +113,24 @@ class MWin:
                 slice_dict.update({i: slice[i]})
             results = Parallel(n_jobs=self.threads)(delayed(self.split_moving_window)(arr, arr2, sl)
                                  for sl in slice_dict.items())
-        sim_list = []
-        for i in range(len(results)):
-            sim_list += results[i]
-        self.sim = fsum(sim_list) / (self.i * self.j)
 
         vector = []
         for i in range(len(results)):
             vector += results[i]
         vector_arr = np.array(vector)
-        self.matrix = vector_arr.reshape([self.i, self.j])
+
+        tw = sum(1 for n in vector if n != -1)
+        self.sim = np.sum(vector_arr) / (tw)
+
+        mat = vector_arr.reshape([self.i, self.j])
+        self.matrix = np.ma.masked_where(mat == -1,
+                                    mat,
+                                    copy=True)
+
 
         return results
 
-    def plot(self, cmap="PiYG"):
+    def plot(self, cmap="Greys"):
 
         plt.imshow(self.matrix, cmap=cmap)
         plt.show()
@@ -143,8 +138,8 @@ class MWin:
 if __name__ == '__main__':
     # arr1 = np.random.randint(2, size=(753, 200))
     # arr2 = np.random.randint(2, size=(753, 200))
-    x = "/home/owen/Data/mwin/nlcd_L_2016.tif"
-    y = "/home/owen/Data/mwin/nlcd_L_2013.tif"
+    x = "/home/owen/Data/mwin/nan_2016.tif"
+    y = "/home/owen/Data/mwin/nan_2013.tif"
 
     data = load_data(x, y)
     sh = data.shape
@@ -154,8 +149,8 @@ if __name__ == '__main__':
     out_dict = {}
     out_times = []
     
-    t = 10
-    w = 3
+    t = 3
+    w = 3  
 
     # t = int(input("Threads: "))
     # w = int(input("Window: "))
@@ -163,6 +158,6 @@ if __name__ == '__main__':
     mw = MWin(t, sh, w)
     test = mw.fit(data.arrays[0], data.arrays[1])
     end = time.time() - start
-    out_times.append(end)
     print(mw.sim)
+    mw.plot(cmap="magma")
 
