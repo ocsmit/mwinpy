61b2b579249ce3288d52b0b17d19dff8533fb408
ocsmit <owen8461@protonmail.com>
2020-10-28 11:13:02 -0400

    Reformat to incorporate raster loading and automated NODATA reading. Can still take in just numpy arrays. Is this better? I dont know. Still need to tinker with it all

--- mwinpy/multi_mw.py
+++ mwinpy/multi_mw.py
@@ -9,38 +9,16 @@ from osgeo import gdal
 from math import fsum, sqrt
 from joblib import Parallel, delayed, dump, load
 
-class load_data:
-
-    def __init__(self, raster1, raster2):
-        path1 = gdal.Open(x)
-        path2 = gdal.Open(y)
-        self.arrays = path1.GetRasterBand(1).ReadAsArray(), \
-                      path2.GetRasterBand(1).ReadAsArray()
-        self.shape = self.arrays[0].shape
-
 
 class MWin:
 
-    def __init__(self, threads, shape, window, nodata=128):
+    def __init__(self, threads, window):
 
-        self.i, self.j = shape
-        self.nodata = nodata
         self.w = window
         self.threads = threads
-        self.cluster = self.i // threads
-        self.rem = self.i % threads
         self.__split_itr = []
         self.vector = []
         self.__d = window // 2
-        # Create inital list of starts and stops for the split
-        itera = 0
-        for f in range(threads):
-            if f == threads - 1:
-                self.__split_itr.append((self.cluster) + itera +
-                                        (self.rem))
-            else:
-                self.__split_itr.append((self.cluster) + itera)
-            itera += (self.cluster)
 
     def __split(self):
         # Create split ranges based of intialist of split numbers
@@ -82,7 +60,6 @@ class MWin:
 
         return vector
 
-
     def split_moving_window(self, arr1, arr2, sl):
         w = (((self.w * 2) + 1) ** 2)
         arr1_sl, arr2_sl = arr1[sl[1][0]:sl[1][1],], arr2[sl[1][0]:sl[1][1],]
@@ -102,17 +79,48 @@ class MWin:
                     vector.append(self.__mw(arr1_sl, arr2_sl, ii, j, w))
         return vector
 
-    def fit(self, arr, arr2):
+    def fit(self, x, y, nodata=None):
+
+        if type(x) and type(y) == str:
+            path1 = gdal.Open(x)
+            path2 = gdal.Open(y)
+            arr1 = path1.GetRasterBand(1).ReadAsArray()
+            arr2 = path2.GetRasterBand(1).ReadAsArray()
+
+            self.nodata = path1.GetRasterBand(1).GetNoDataValue()
+        elif type(x) and type(y) == np.ndarray:
+            arr1 = x
+            arr2 = y
+            self.nodata = nodata
+        else:
+            raise TypeError("Inputs x and y must be valid file paths or numpy arrays.")
+
+        self.i, self.j = arr1.shape
+        # Create inital list of starts and stops for the split
+
+        self.cluster = self.i // self.threads
+        self.rem = self.i % self.threads
+
+        itera = 0
+        for f in range(self.threads):
+            if f == self.threads - 1:
+                self.__split_itr.append((self.cluster) + itera +
+                                        (self.rem))
+            else:
+                self.__split_itr.append((self.cluster) + itera)
+            itera += (self.cluster)
+
         if self.threads == 1:
             results = []
-            results.append(self.moving_window(arr, arr2))
+            results.append(self.moving_window(arr1, arr2))
         else:
             slice = self.__split()
             slice_dict = {}
             for i in range(len(slice)):
                 slice_dict.update({i: slice[i]})
-            results = Parallel(n_jobs=self.threads)(delayed(self.split_moving_window)(arr, arr2, sl)
-                                 for sl in slice_dict.items())
+            results = Parallel(n_jobs=self.threads)(delayed(
+                      self.split_moving_window)(arr1, arr2, sl)
+                      for sl in slice_dict.items())
 
         vector = []
         for i in range(len(results)):
@@ -140,9 +148,6 @@ if __name__ == '__main__':
     # arr2 = np.random.randint(2, size=(753, 200))
     x = "/home/owen/Data/mwin/nan_2016.tif"
     y = "/home/owen/Data/mwin/nan_2013.tif"
-
-    data = load_data(x, y)
-    sh = data.shape
     
     w = [3, 13, 23, 33, 43, 53, 63]
     t = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
@@ -155,8 +160,8 @@ if __name__ == '__main__':
     # t = int(input("Threads: "))
     # w = int(input("Window: "))
     start = time.time()
-    mw = MWin(t, sh, w)
-    test = mw.fit(data.arrays[0], data.arrays[1])
+    mw = MWin(t, w)
+    test = mw.fit(x, y)
     end = time.time() - start
     print(mw.sim)
     mw.plot(cmap="magma")
