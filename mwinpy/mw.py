Fix neighbor issues where all cells within 'd' value of the top and left edge of the matrix would return the wrong values and result and a similarity value of 1.

mwinpy/mw.py
@@ -38,7 +38,7 @@ class MWin:
             Number of jobs to run in parallel across the CPU. -1 will use all
             available cores.
         '''
-        self.w = window_size
+        self.__w = window_size
         self.threads = n_jobs
         # Initialize
         self.__split_itr = []
@@ -46,10 +46,28 @@ class MWin:
         # Get d value which indicates the number of cells to move from the
         # original cell for the window size w.
         self.__d = window_size // 2
+        self.verbosity = 0
+        self.time = []
 
-    def __check(self, x, y):
-        if x != y:
-            raise IOError
+    def __verbose(func):
+        def wrapper(self, *args, **kwargs):
+            if self.verbosity == 1:
+                start_time = time.time()
+                func(self, *args, **kwargs)
+                end_time = time.time() - start_time
+                self.time.append(end_time)
+                print(f"{end_time} Seconds")
+            else:
+                func(self, *args, **kwargs)
+        return wrapper
+
+    def __check_types(func):
+        def wrapper(self, x, y, nodata):
+            if type(x) != type(y):
+                raise TypeError("Inputs x and y are not the same type")
+            func(self, x, y, nodata)
+        return wrapper
+
 
     def __split(self):
         '''
@@ -70,8 +88,7 @@ class MWin:
             # Take into account edge artifacts created by loss of contextual
             # data for windows due to split.
             if j == 0:
-                iter_range.append([self.__split_itr[j] - (self.cluster),
-                                   self.__split_itr[j] + self.__d])
+                iter_range.append([0, self.__split_itr[j] + self.__d])
             elif j == self.threads - 1:
                 iter_range.append([self.__split_itr[j] - (self.cluster +
                                   self.rem + self.__d), self.__split_itr[j]])
@@ -82,7 +99,8 @@ class MWin:
 
     def __neighbors(self, arr, i, j):
         '''
-        Computes neighbor vector for window size w
+        Computes neighbor vector for window size w with consideration for outer
+        edges.
 
         Parameters
         ----------
@@ -99,8 +117,11 @@ class MWin:
         n : ndarray
             Cell neighborhood vector
         '''
-        n = arr[i - self.__d:i + self.__d + 1,
-                j - self.__d:j + self.__d + 1].flatten()
+
+        # Use min and max to account for upper x and y neighbors
+        # ranges of cells within the 'd' from the edges
+        n = arr[max(i-self.__d,0):min(i+self.__d + 1,arr.shape[0]),
+                max(j-self.__d,0):min(j+self.__d + 1,arr.shape[1])].flatten()
         return n
 
     def __mw(self, arr1, arr2, ii, j):
@@ -128,7 +149,6 @@ class MWin:
         '''
 
         # number of total cells across two neighborhoods with window size w.
-        w = (((self.w * 2) + 1) ** 2)
 
         # Assign nodata value of -1 which will be ignored in final coefficient
         if arr1[ii][j] and arr2[ii][j] == self.nodata:
@@ -144,7 +164,8 @@ class MWin:
             # Divide number of cells which are different by the total number of
             # cells in the two neighborhoods. If it is 100% similar assign cell
             # the value of 1.
-            similarity = abs((1 - d / w)) if d != 0 else w / w
+            w = (((len(c) * 2) + 1) ** 2)
+            similarity = (1 - d / w) if d != 0 else 1
             return similarity
 
     def moving_window(self, arr1, arr2):
@@ -166,10 +187,9 @@ class MWin:
             List vector which contains similarity values of each cell
 
         '''
-        w = (((self.w * 2) + 1) ** 2)
         vector = []
-        for ii in range(arr1.shape[0]):
-            for j in range(arr1.shape[1]):
+        for ii in range(self.arr1.shape[0]):
+            for j in range(self.arr1.shape[1]):
                 vector.append(self.__mw(arr1, arr2, ii, j))
         return vector
 
@@ -199,21 +219,35 @@ class MWin:
                            arr2[sl[1][0] : sl[1][1], ]
         vector = []
         if sl[0] == 0:
-            for ii in range(arr1_sl.shape[0] - self.__d):
+            for ii in range(0, arr1_sl.shape[0] - self.__d):
                 for j in range(arr1_sl.shape[1]):
                     vector.append(self.__mw(arr1_sl, arr2_sl, ii, j))
-
         elif sl[0] == self.threads - 1:
             for ii in range(self.__d, arr1_sl.shape[0]):
                 for j in range(arr1_sl.shape[1]):
                     vector.append(self.__mw(arr1_sl, arr2_sl, ii, j))
         else:
-            for ii in range(self.__d, arr1_sl.shape[0] - (self.__d)):
+            for ii in range(self.__d, arr1_sl.shape[0] - self.__d):
                 for j in range(arr1_sl.shape[1]):
                     vector.append(self.__mw(arr1_sl, arr2_sl, ii, j))
         return vector
 
-    def fit(self, x, y, nodata=None):
+    @__check_types
+    def __load_data(self, x, y, nodata):
+
+        if type(x) and type(y) == str:
+            path1 = gdal.Open(x)
+            path2 = gdal.Open(y)
+            self.arr1 = path1.GetRasterBand(1).ReadAsArray()
+            self.arr2 = path2.GetRasterBand(1).ReadAsArray()
+            self.nodata = path1.GetRasterBand(1).GetNoDataValue()
+        elif type(x) and type(y) == np.ndarray:
+            self.arr1 = x
+            self.arr2 = y
+            self.nodata = nodata
+
+    @__verbose
+    def fit(self, x, y, nodata=None, ):
         '''
         Computes moving window algorithm.
 
@@ -228,22 +262,12 @@ class MWin:
             Only needs to be set if x and y are ndarrays, otherwise the nodata
             value is read from the raster datasets.
         '''
+        self.__load_data(x, y, nodata)
 
-        if type(x) and type(y) == str:
-            path1 = gdal.Open(x)
-            path2 = gdal.Open(y)
-            arr1 = path1.GetRasterBand(1).ReadAsArray()
-            arr2 = path2.GetRasterBand(1).ReadAsArray()
-            self.nodata = path1.GetRasterBand(1).GetNoDataValue()
-            self.__check(self.nodata, path2.GetRasterBand(1).GetNoDataValue())
-        elif type(x) and type(y) == np.ndarray:
-            arr1 = x
-            arr2 = y
-            self.nodata = nodata
-        else:
-            raise TypeError("Inputs x and y must be valid file paths or numpy arrays.")
+        if self.arr1.shape != self.arr2.shape:
+            raise Exception("Shape of x and y is not the same")
 
-        self.i, self.j = arr1.shape
+        self.i, self.j = self.arr1.shape
         # Create initial list of starts and stops for the split
 
         self.cluster = self.i // self.threads
@@ -260,14 +284,14 @@ class MWin:
 
         if self.threads == 1:
             results = []
-            results.append(self.moving_window(arr1, arr2))
+            results.append(self.moving_window(self.arr1, self.arr2))
         else:
             slice = self.__split()
             slice_dict = {}
             for i in range(len(slice)):
                 slice_dict.update({i: slice[i]})
             results = Parallel(n_jobs=self.threads)(delayed(
-                      self.split_moving_window)(arr1, arr2, sl)
+                      self.split_moving_window)(self.arr1, self.arr2, sl)
                       for sl in slice_dict.items())
 
         vector = []
@@ -283,8 +307,12 @@ class MWin:
                                     mat,
                                     copy=True)
 
-
         return results
+
+    def fit_multi():
+
+
+        return
 
     def plot(self, cmap="Greys"):
 
@@ -294,8 +322,8 @@ class MWin:
 if __name__ == '__main__':
     # arr1 = np.random.randint(2, size=(753, 200))
     # arr2 = np.random.randint(2, size=(753, 200))
-    x = "/home/owen/Data/mwin/2016.tif"
-    y = "/home/owen/Data/mwin/2013.tif"
+    x = "/home/owen/Data/mwin/nan_2016.tif"
+    y = "/home/owen/Data/mwin/nan_2013.tif"
 
     w = [3, 13, 23, 33, 43, 53, 63]
     t = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
@@ -303,12 +331,12 @@ if __name__ == '__main__':
     out_times = []
 
     t = 3
-    w = 5
+    w = 3
 
     # t = int(input("Threads: "))
     # w = int(input("Window: "))
     start = time.time()
-    mw = MWin(t, w)
+    mw = MWin(w, t)
     test = mw.fit(x, y)
     end = time.time() - start
     print(mw.sim)